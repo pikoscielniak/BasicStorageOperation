ef8b61324836d1fa8e6b3449b015d0e56500e7b4
piotrekbass <piotrek.koscielniak@gmail.com>
2014-08-25 22:06:54 +0200

    init

---
+++ BasicStorageOperation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace BasicStorageOperation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var environment = File.ReadAllLines("../../environment.txt");
+			string name = environment[0];
+			string key = environment[1];
+
+		}
+	}
+}
