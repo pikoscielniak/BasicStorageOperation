aa23626e5eb2f2eeb74f3d6e8b9c982678dc7494
piotrekbass <piotrek.koscielniak@gmail.com>
2014-08-27 23:20:27 +0200

    data contracts

---
+++ DataContracts/LapData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DataContracts
+{
+	[DataContract(Namespace = "http://somenamespace.com")]
+	public class LapData
+	{
+		[DataMember(Order = 1)]
+		public string PlayerName { get; set; }
+
+		[DataMember(Order = 4)]
+		public string LapId { get; set; }
+
+		[DataMember(Order = 5)]
+		public DateTime GameStarTime { get; set; }
+
+		[DataMember(Order = 6)]
+		public int LapTimeMs { get; set; }
+
+		[DataMember(Order = 7)]
+		public List<int> SectorTimesMs { get; set; }
+
+		public LapData()
+		{
+			SectorTimesMs = new List<int>();
+			GameStarTime = DateTime.UtcNow;
+		}
+	}
+}
---
+++ DataContracts/SerializationUtils.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+
+namespace DataContracts
+{
+	public static class SerializationUtils
+	{
+		public static string SerializeToString<T>(T objectToSerialize)
+		{
+			using (var memStm = new MemoryStream())
+			{
+				var serializer = new DataContractSerializer(typeof(T));
+				var sb = new StringBuilder();
+				using (var writer = XmlWriter.Create(sb))
+				{
+					serializer.WriteObject(writer, objectToSerialize);
+					writer.Flush();
+					return sb.ToString();
+				}
+			}
+		}
+
+		public static T Deserialize<T>(string rawXml)
+		{
+			using (var reader = XmlReader.Create(new StreamReader(rawXml)))
+			{
+				var formatter = new DataContractSerializer(typeof(T));
+				return (T)formatter.ReadObject(reader);
+			}
+		}
+
+		public static byte[] SerializeToByteArray<T>(T obj)
+		{
+			var serializer = new DataContractSerializer(typeof(T));
+			var stream = new MemoryStream();
+			using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+			{
+				serializer.WriteObject(writer, obj);
+			}
+			return stream.ToArray();
+		}
+
+		public static T Deserialize<T>(byte[] data)
+		{
+			var serializer = new DataContractSerializer(typeof(T));
+			using (var stream = new MemoryStream(data))
+			using (var reader = XmlDictionaryReader.CreateBinaryReader(stream, XmlDictionaryReaderQuotas.Max))
+			{
+				return (T)serializer.ReadObject(reader);
+			}
+		}
+	}
+}
