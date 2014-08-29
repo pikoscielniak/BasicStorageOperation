449af9458128b566b447bacf500aa42586907b2d
piotrekbass <piotrek.koscielniak@gmail.com>
2014-08-29 23:20:25 +0200

    adding items to azure table

---
+++ AzureTableStorage.AzureDataAccess/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureTableStorage.AzureDataAccess
+{
+	public class Product : TableEntity	
+	{
+		public string Name { get; set; }
+		public string Color { get; set; }
+		public double ListPrice { get; set; }
+		public string Model { get; set; }	
+	}
+
+	public class ProductAccess
+	{
+		private readonly CloudTable _table;
+
+		public ProductAccess()
+		{
+			var connectionString = File.ReadAllText("../../environment.txt");
+
+			var account = CloudStorageAccount.Parse(connectionString);
+			var client = account.CreateCloudTableClient();
+			_table = client.GetTableReference("TableLabProducts");
+			_table.CreateIfNotExists();
+		}
+
+		public void Insert(Product product)
+		{
+			var operation = TableOperation.Insert(product);
+			_table.Execute(operation);
+		}
+	}
+}
---
+++ BasicStorageOperation/AzureTableStorage.TestClient/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using AzureTableStorage.AzureDataAccess;
+
+namespace AzureTableStorage.TestClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var watch = Stopwatch.StartNew();
+			Console.WriteLine("Press enter to start");
+			Console.ReadLine();
+
+			InsertProducts();
+
+			watch.Stop();
+			Console.WriteLine("Time: " + watch.Elapsed.ToString("mm\\:ss\\.fff"));
+			Console.ReadLine();
+		}
+
+		private static void InsertProducts()
+		{
+			var productAccess = new ProductAccess();
+			for (var i = 1001; i <= 1100; i++)
+			{
+				var product = GetRandomProduct(i);
+				productAccess.Insert(product);
+				Console.WriteLine("Product: " + i);
+			}
+		}
+
+		private static Product GetRandomProduct(int number)
+		{
+			return new Product
+			{
+				PartitionKey = RandomCategory(),
+				RowKey = number.ToString(CultureInfo.InvariantCulture),
+				Name = RandomString(10),
+				Color = RandomString(6),
+				Model = RandomString(4),
+				ListPrice = Random.NextDouble() * 1000,
+			};
+		}
+
+		private static string RandomCategory()
+		{
+			return Categories[Random.Next(Categories.Count)];
+		}
+
+		private static readonly Random Random = new Random((int)DateTime.Now.Ticks);
+		private static readonly IList<string> Categories = new List<string> { "Food", "Clothes", "Toys", "Forniture" };
+
+		private static string RandomString(int size)
+		{
+			var builder = new StringBuilder();
+			for (int i = 0; i < size; i++)
+			{
+				var ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * Random.NextDouble() + 65)));
+				builder.Append(ch);
+			}
+
+			return builder.ToString();
+		}
+	}
+}
